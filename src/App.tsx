609ca3e1ac8b0eef023cf9a61c13f7a05624f469
Qingqing <qingqing.dai94@gmail.com>
2023-01-17 10:09:38 +0100

    display by details

--- src/App.tsx
+++ src/App.tsx
@@ -3,51 +3,33 @@ import './App.css';
 import Section from './components/Section';
 import { Blog } from './types/types';
 
+const sections = ['history', 'crime', 'fiction', 'magical', 'mystery'];
 
 function App() {
   const [allBlogs, setAllBlogs] = useState<Array<Blog>>([]);
-  const [blogs, setBlogs] = useState<Array<Blog>>([]);
-  const [section, setSection] = useState("history");
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    e.preventDefault();
-    const value = e.target.value;
-    setSection(value);
 
-  }
   
   useEffect(() => {
     const getData = async () => {
       const results = await fetch('https://dummyjson.com/posts');
       const data = await results.json();
       setAllBlogs(data.posts);
-      const temp = allBlogs.filter(b => b.tags.includes('history'));
-      setBlogs(temp);
     }
 
     getData();
   }, []);
 
-  useEffect(() => {
-    const temp = allBlogs.filter(b => b.tags.includes(section));
-    setBlogs(temp);
-  }, [section]);
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>Tiny Blog</h2>
-        <div>
-          <select value={section} onChange={handleChange}>
-            <option value="history">History</option>
-            <option value="crime">crime</option>
-            <option value="fiction">fiction</option>
-            <option value="magical">magical</option>
-            <option value="mystery">mystery</option>
-          </select>
-          <p>{section}</p>
-        </div>
       </header>
-      <Section blogs={blogs} />
+
+      {sections.map(s => {
+        const bs = allBlogs.filter(b => b.tags.includes(s));
+        return <Section blogs={bs} section={s}/>
+      })}
     </div>
   );
 }
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Blog } from '../types/types';
 import Card from './Card';
 interface ISectionProps {
-    blogs: Blog[]
+    blogs: Blog[],
+    section: string
 }
 
-let Section = ({blogs}: ISectionProps) => {
+let Section = ({blogs, section}: ISectionProps) => {
   return (
     <div>
+      <details>
+        <summary>{section}</summary>
         {blogs.map(b => <Card blog={b} />)}
+      </details>
+        
     </div>
     
   )
